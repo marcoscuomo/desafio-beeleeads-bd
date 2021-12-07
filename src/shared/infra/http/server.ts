b5869bf411bd466587e8d7670b129bdb6bd58c08
Marcos Souza <bytecore@gmail.com>
2021-12-07 08:20:46 -0300

    Will update port

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,5 @@
 import { app } from './app';
 
-app.listen(3333, () => console.log("server is running!"));
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => console.log("server is running!"));
